567323457b62bee01bcb9d0b314f471200d3d395
Daniel Bartolini <117309143+djbartolini@users.noreply.github.com>
2024-05-22 17:45:35 -0400

    Update stem_chart.py

--- stem_chart.py
+++ stem_chart.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as mpl 
+import numpy
 
-x = [1, 2, 3, 4, 5]
-y = [1, 4, 9, 16, 25]
-z = [1, 8, 27, 64, 125]
+x = numpy.linspace(0.1, 2 * numpy.pi, 41)
+y = numpy.exp(numpy.sin(x))
 
-figure = mpl.figure()
+mpl.stem(x, y, use_line_collection = True)
+mpl.title("Stem graph")
 
-ax = mpl.axes(projection = '3d')
-
-as.plot3D(z, y, x)
+# Legend
+mpl.legend(["Example figure"])
+mpl.show()
