3531ddaefb9a0f805d0b0d12e28898146c7adf93
Daniel Bartolini <117309143+djbartolini@users.noreply.github.com>
2024-05-22 17:22:38 -0400

    Create line_graph.py

---
+++ line_graph.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as mpl 
+
+# Line Graph
+
+x = [3, 1, 3]
+y = [3, 2, 1]
+
+# Table
+mpl.plot(x, y)
+mpl.title("Line Graph")
+
+# Legend
+mpl.legend(["Example figure"])
+mpl.show()
